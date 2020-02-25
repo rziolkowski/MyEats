db52e3455cbcf45d96585f725a691f04323412a8
rziolkowski <ryan.ziolkowski@aol.com>
2020-02-25 11:21:15 -0600

    Styling on map now done in separate css file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import {AgmCoreModule} from "@agm/core";
+import { AgmCoreModule } from "@agm/core";
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
@@ -9,10 +9,6 @@ import { MapComponent } from './map/map.component';
 @NgModule({
   imports:      [ BrowserModule, FormsModule, AgmCoreModule.forRoot({
       apiKey: 'AIzaSyAEU5775h5usxBZTtjiROet4ZoRsqk_PbE'
-      /* apiKey is required, unless you are a 
-      premium customer, in which case you can 
-      use clientId 
-      */
     })
   ],
   declarations: [ AppComponent, MapComponent ],
--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -3,15 +3,16 @@ import { Component } from "@angular/core";
 @Component({
   selector: 'my-map',
   styleUrls: [ './map.component.css' ],
-  styles: ['agm-map { height: 300px; width: 300px; float: right; }'],
   template: `
     <agm-map [latitude]='latitude' [longitude]='longitude' 
-      [mapTypeId]='mapType'>
+      [mapTypeId]='mapType' [zoom]='zoom'>
     </agm-map>
   `
 })
+
 export class MapComponent {
-  latitude = -28.68352;
-  longitude = -147.20785;
+  latitude = 41.773671;
+  longitude =  -88.142937;
   mapType = 'hybrid';
+  zoom = 15;
 }
