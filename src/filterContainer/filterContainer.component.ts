989a59a429bd639fd672e7d4113c79ca8c3cd48f
rziolkowski <ryan.ziolkowski@aol.com>
2020-03-19 10:02:18 -0500

    Revert "Add files via upload"
    This reverts commit a29e1049d72f9de532327e9abf05a62bc6fed9c5.

--- src/filterContainer/filterContainer.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'my-filters',
-  templateUrl: './filterContainer.component.html',
-  styleUrls: [ './filterContainer.component.css' ]
-})
-export class FilterContainerComponent {
-}
