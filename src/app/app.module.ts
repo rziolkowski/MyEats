9199e21a2354ccdac65cea05417aabebe65aaa2f
rziolkowski <ryan.ziolkowski@aol.com>
2020-03-19 10:07:56 -0500

    Added filter container framework

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,14 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { MapComponent } from './map/map.component';
 import { RestaurantContainerComponent } from './restaurantContainer/restaurantContainer.component'
+import { FilterContainerComponent } from './filterContainer/filterContainer.component'
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule, AgmCoreModule.forRoot({
       apiKey: 'AIzaSyAEU5775h5usxBZTtjiROet4ZoRsqk_PbE'
     })
   ],
-  declarations: [ AppComponent, MapComponent, RestaurantContainerComponent ],
+  declarations: [ AppComponent, MapComponent, RestaurantContainerComponent, FilterContainerComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
