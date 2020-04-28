e14dcf9683de938c19175dfa452f6c6c704f107e
rziolkowski <ryan.ziolkowski@aol.com>
2020-04-28 03:52:32 -0500

    Some style changes, and added pin drop functionality to the map

--- src/app/home/map/map.component.ts
+++ src/app/home/map/map.component.ts
@@ -5,7 +5,15 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: [ './map.component.css' ],
   template: `
     <agm-map [latitude]='latitude' [longitude]='longitude' 
-      [mapTypeId]='mapType' [zoom]='zoom'>
+      [mapTypeId]='mapType' [zoom]='zoom' (mapClick)="addMarker($event.coords.lat, $event.coords.lng)">
+      <agm-marker
+        [latitude]="marker.lat"
+        [longitude]="marker.lng"
+        [opacity]="marker.alpha"
+        [markerDraggable]="true"
+        (markerClick)="selectMarker($event)"
+      >
+      </agm-marker>
     </agm-map>
   `
 })
@@ -13,8 +21,23 @@ import { Component, OnInit } from "@angular/core";
 export class MapComponent implements OnInit{
   mapType = 'hybrid';
   zoom = 15;
+  selectedMarker;
+  marker;
+  latitude = 41.7727;
+  longitude = 88.1440;
 
- ngOnInit() {
+  addMarker(lat: number, lng: number) {
+    this.marker = { lat, lng, alpha: 0.4 };
+  }
+
+  selectMarker(event) {
+    this.selectedMarker = {
+      lat: event.latitude,
+      lng: event.longitude
+    };
+  }
+
+  ngOnInit() {
     this.setCurrentLocation();
   }
  
@@ -25,6 +48,7 @@ export class MapComponent implements OnInit{
         this.latitude = position.coords.latitude;
         this.longitude = position.coords.longitude;
         this.zoom = 15;
+        this.marker = { lat: this.latitude, lng: this.longitude, alpha: 0.4 }
       });
     }
   }
